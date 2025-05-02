06a6ec346de89ff2fcb7c1746e8f90f1721a6fb5
Mohit Raj <mohitraj12099@gmail.com>
2025-05-02 13:56:36 +0000

    remove unused file

--- components/shared/Search.jsx
+++ components/shared/Search.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect, Suspense, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useLazyGetSummaryQuery } from "@/utils/services/article";
 import Loading from "../../app/loading";
 import { motion } from "framer-motion";
