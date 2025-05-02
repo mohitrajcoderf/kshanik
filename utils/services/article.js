9b65d5abb9b9f662976322649e5732facea4928b
Mohit Raj <mohitraj12099@gmail.com>
2025-05-02 12:45:06 +0000

    fix: bugs in article api

--- utils/services/article.js
+++ utils/services/article.js
@@ -1,21 +1,20 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const articleApi = createApi({
     reducerPath: "articleApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
-        headers: {
-            "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-            "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
-        },
+      baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+      headers: {
+        "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+        "X-RapidAPI-Host": "article-extractor-and-summarizer.p.rapidapi.com",
+      },
     }),
-
     endpoints: (builder) => ({
-        getSummery: builder.query({
-            query: (params) => 
-                `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
-        }),
+      getSummary: builder.query({
+        query: (params) =>
+          `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+      }),
     }),
-});
-
-export const { useLazyGetSummaryQuery } = articleApi;
+  });
+  
+  export const { useLazyGetSummaryQuery } = articleApi;  
