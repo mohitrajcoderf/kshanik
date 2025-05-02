b96c3d069deb8eba70b2ee539621041ebf6b7c94
Mohit Raj <mohitraj12099@gmail.com>
2025-05-02 11:09:12 +0000

    first-draft

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        RAPID_API_KEY: "219e22e76dmshbb7df3bac2fb9dcp1587ecjsnb6b6fb3fe72a",
+    },
+};
+
+export default nextConfig;
